01848f8a9a4c125e7ca2b97947c19a66c536e9da
Thomas Fahrmeyer <thomas.fahrmeyer.ext@zeiss.com>
2019-07-11 16:54:48 +0200

    clean up for exec-osgi-java plugin, removed old execute method
    - use the AppClasspathLauncher for all the logic

--- maven-osgi-exec-plugin/src/main/java/at/bestsolution/maven/osgi/exec/MVNJavaOSGiLaunch.java
+++ maven-osgi-exec-plugin/src/main/java/at/bestsolution/maven/osgi/exec/MVNJavaOSGiLaunch.java
@@ -12,37 +12,27 @@ package at.bestsolution.maven.osgi.exec;
 
 import at.bestsolution.maven.osgi.support.AppClasspathLauncher;
 import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.plugins.annotations.ResolutionScope;
-import org.apache.maven.shared.utils.cli.CommandLineException;
-import org.apache.maven.shared.utils.cli.CommandLineUtils;
 
 import java.io.File;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLClassLoader;
-import java.nio.file.Path;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.jar.Manifest;
-import java.util.stream.Collectors;
-
-import static at.bestsolution.maven.osgi.support.Constants.OSGI_FRAMEWORK_EXTENSIONS;
 
 @Mojo(name="exec-osgi-java", requiresDependencyResolution = ResolutionScope.COMPILE_PLUS_RUNTIME)
 public class MVNJavaOSGiLaunch extends MVNBaseOSGiLaunchPlugin {
 
-	private static final String EQUINOX_LAUNCHER_MAIN_CLASS = "org.eclipse.equinox.launcher.Main";
 
 	@SuppressWarnings("unused")
 	@Parameter(property = "exec.args")
 	private String commandlineArgs;
+
 	private AppClasspathLauncher launcher;
 
-	public void execute() throws MojoExecutionException, MojoFailureException {
+	public void execute() throws MojoExecutionException {
         if (!configPath.isEmpty()) {
 			File configFile = new File(configPath);
 			if (!configFile.exists()) {
@@ -65,68 +55,6 @@ public class MVNJavaOSGiLaunch extends MVNBaseOSGiLaunchPlugin {
         return Arrays.asList(commandlineArgs.split(" "));
     }
 
-    public void executeOld() throws MojoExecutionException, MojoFailureException {
-		Set<Path> extensionPaths = new HashSet<>();
-		Path ini = generateConfigIni(extensionPaths);
-		
-		Optional<URL> launcherJar = project.getArtifacts().stream()
-				.filter(a -> "org.eclipse.equinox.launcher".equals(a.getArtifactId())).findFirst()
-				.map(a -> {
-					try {
-						return a.getFile().toURL();
-					} catch (MalformedURLException e) {
-						throw new RuntimeException(e);
-					}
-				});
-		
-		URLClassLoader l = new URLClassLoader( new URL[] { launcherJar.get() } );
-		
-		List<String> cmd = new ArrayList<>();
-		cmd.add("-configuration");
-		cmd.add("file:" + ini.toString());
-		cmd.addAll(programArguments);
-
-		appendCommandLineArgumentsTo(cmd);
-
-		if( vmProperties.containsKey(OSGI_FRAMEWORK_EXTENSIONS) ) {
-			String extensionClasspath = extensionPaths.stream().map(Path::toString).collect(Collectors.joining(",","file:",""));
-			if( ! extensionClasspath.trim().isEmpty() ) {
-				vmProperties.put("osgi.frameworkClassPath",".," + extensionClasspath);
-			}
-		}
-
-		System.getProperties().putAll(vmProperties);
-		
-		Thread t = new Thread() {
-			public void run() {
-				try {
-					Class<?> cl = getContextClassLoader().loadClass(EQUINOX_LAUNCHER_MAIN_CLASS);
-					Method m = cl.getDeclaredMethod("main", String[].class);
-					m.invoke(null, new Object[] {cmd.toArray(new String[0])});
-
-				} catch (ClassNotFoundException | NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
-					logger.error("Can not invoke main-method for " + EQUINOX_LAUNCHER_MAIN_CLASS, e);
-				}
-			}
-		};
-		t.setContextClassLoader(l);
-		t.start();
-		try {
-			t.join();
-		} catch (InterruptedException e) {
-			logger.error("Error on waiting for Equinox launcher to finish.", e);
-		}
-	}
-
-	private void appendCommandLineArgumentsTo(List<String> cmds) throws MojoExecutionException {
-		if (commandlineArgs != null) {
-			try {
-				cmds.addAll(Arrays.asList(CommandLineUtils.translateCommandline(commandlineArgs)));
-			} catch (CommandLineException e) {
-				throw new MojoExecutionException(e.getMessage());
-			}
-		}
-	}
 
 	@Override
 	protected Integer getStartLevel(Manifest m) {
